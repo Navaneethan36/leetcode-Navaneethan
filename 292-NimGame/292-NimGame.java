6a98caf2ceb080b98889cbfd68fbfe548d1bf43c
Navaneethan36 <navaneet411@gmail.com>
2026-08-12 11:15:49 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 8/12/2026, 11:15:50 AM
+class Solution {
+    public boolean canWinNim(int n) {
+        
+        if(n%4==0)
+           return false;
+        else 
+           return true;   
+    }
+}
