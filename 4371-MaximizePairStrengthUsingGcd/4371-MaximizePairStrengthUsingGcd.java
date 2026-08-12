b2e509ca5c2fbd5d2c3597968e082adfa1bc037a
Navaneethan36 <navaneet411@gmail.com>
2026-08-12 11:13:52 +0530

    Create 4371-MaximizePairStrengthUsingGcd

---
+++ 4371-MaximizePairStrengthUsingGcd/4371-MaximizePairStrengthUsingGcd.java
@@ -0,0 +1,24 @@
+// Last updated: 8/12/2026, 11:13:53 AM
+class Solution {
+    public long maxPairStrength(int[] nums) {
+     long ans = 0;
+
+        for(int i =0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                long g = gcd(nums[i],nums[j]);
+                long strength = (1L * nums[i]*nums[j])/(g*g);
+                ans = Math.max(ans,strength);
+    }
+}
+        return ans;
+    }
+    private long gcd(long a,long b)
+    {
+        while(b != 0){
+            long t=a%b;
+            a=b;
+            b=t;
+        }
+        return a;
+    }
+}
